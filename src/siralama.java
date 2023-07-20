1c4c15db5f01fac98864d24e337682211260212c
simgedeemir <simged37@gmail.com>
2023-07-20 18:16:37 +0300

    sayıları sıralama

---
+++ src/siralama.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+public class siralama {
+    public static void main(String[] args) {
+        Scanner input= new Scanner(System.in);
+        int a,b,c;
+
+        System.out.print("1.Sayıyı giriniz:");
+        a= input.nextInt();
+
+        System.out.print("2.Sayıyı giriniz:");
+        b= input.nextInt();
+
+        System.out.print("3.Sayıyı giriniz:");
+        c= input.nextInt();
+
+        if(a<b && a<c)
+        {
+            if(b<c)
+            {
+                System.out.println("a<b<c");
+            }
+            else {
+                System.out.println("a<c<b");
+            }
+        }else if(b<a && b<c)
+        {
+            if(a<c)
+            {
+                System.out.println("b<a<c");
+            }else{
+                System.out.println("b<c<a");
+            }
+        }else {
+            if (a < b) {
+                System.out.println("c<a<b");
+            } else {
+                System.out.println("c<b<a");
+            }
+        }
+
+    }
+}
