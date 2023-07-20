40da977a31811ec6dd6b572ffd570922e7acc2b1
simgedeemir <simged37@gmail.com>
2023-07-20 19:09:31 +0300

    çin zodyağı

---
+++ src/Cinzodyagi.java
@@ -0,0 +1,52 @@
+import java.util.Scanner;
+public class Cinzodyagi {
+    public static void main(String[] args) {
+        int yil,kalan;
+        Scanner input=new Scanner(System.in);
+
+        System.out.print("Doğum Yılınızı Giriniz :");
+        yil= input.nextInt();
+        kalan=yil%12;
+
+        switch (kalan){
+            case 0:
+                System.out.println("Maymun");
+                break;
+            case 1:
+                System.out.println("Horoz");
+                break;
+            case 2:
+                System.out.println("Köpek");
+                break;
+            case 3:
+                System.out.println("Domuz");
+                break;
+            case 4:
+                System.out.println("Fare");
+                break;
+            case 5:
+                System.out.println("Öküz");
+                break;
+            case 6:
+                System.out.println("Kaplan");
+                break;
+            case 7:
+                System.out.println("Tavşan");
+                break;
+            case 8:
+                System.out.println("Ejderha");
+                break;
+            case 9:
+                System.out.println("Yılan");
+                break;
+            case 10:
+                System.out.println("At");
+                break;
+            case 11:
+                System.out.println("Koyun");
+                break;
+
+        }
+
+    }
+}
