a5a24b9951e71221442e37562719971a8eb68ffd
simgedeemir <simged37@gmail.com>
2023-07-23 14:00:29 +0300

    polindromsayi

---
+++ src/polindromsayilar.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class polindromsayilar {
+
+    static boolean isPolindrome(int number){
+        int temp=number,reverseNumber=0,lastNumber;
+        while (temp!=0){
+
+            lastNumber=temp%10;
+            reverseNumber=(reverseNumber*10)+lastNumber;
+            temp /=10;
+
+        }
+        if(number==reverseNumber)
+                return true;
+        else
+            return false;
+    }
+    public static void main(String[] args) {
+
+        System.out.println(isPolindrome(247));
+    }
+}
