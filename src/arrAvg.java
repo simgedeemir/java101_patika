8e0e33827af45519cbd57b1d2fccec726bb79acb
simgedeemir <simged37@gmail.com>
2023-07-31 18:08:28 +0300

    array ortalama bulma

---
+++ src/arrAvg.java
@@ -0,0 +1,12 @@
+public class arrAvg {
+    public static void main(String[] args) {
+        int[] list= {15,6,7,12,7,9,3};
+        double avg;
+        double sum=0.0;
+        for(int i=0;i< list.length;i++){
+            sum +=list[i];
+        }
+        avg=sum/ list.length;
+        System.out.println(avg);
+    }
+}
